9312944671dd141b9934c5728195554e106a2676
John <jshar15089@gmail.com>
2023-08-15 04:21:27 -0400

    added button component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-function App(){
-    return(
-        <div>
-            App
-        </div>
-    )
+import Button from './Button';
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Button success primary rounded outline>
+          Click me!!
+        </Button>
+      </div>
+      <div>
+        <Button danger outline>
+          Buy Now!
+        </Button>
+      </div>
+      <div>
+        <Button warning>See Deal!</Button>
+      </div>
+      <div>
+        <Button secondary outline>
+          Hide Ads!
+        </Button>
+      </div>
+      <div>
+        <Button secondary rounded>
+          Something!
+        </Button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
