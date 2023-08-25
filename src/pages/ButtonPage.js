163f5242917cb6fc5394b9644ca35d6c167648ac
John <jshar15089@gmail.com>
2023-08-25 04:39:11 -0400

    code refactored

--- src/pages/ButtonPage.js
+++ src/pages/ButtonPage.js
@@ -1,7 +1,7 @@
-import Button from './components/Button';
+import Button from '../components/Button';
 import {GoBell, GoCloud, GoDatabase} from "react-icons/go"
 
-function App() {
+function ButtonPage() {
 
   function handleClick(){
     console.log("Click")
@@ -40,4 +40,4 @@ function App() {
   );
 }
 
-export default App;
+export default ButtonPage;
