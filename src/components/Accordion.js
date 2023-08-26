394ed69cf56f0e20340571fa49ad33bce7164425
John <jshar15089@gmail.com>
2023-08-26 05:48:19 -0400

    completed accordion component

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,21 +1,39 @@
 import { useState } from "react"
+import { GoChevronDown, GoChevronLeft } from "react-icons/go"
 
 function Accordion({items}){
-const [expandedIndex, setExpandedIndex] = useState(0)
+const [expandedIndex, setExpandedIndex] = useState(-1)
+
+const handleClick = (nextIndex) =>{
+    setExpandedIndex((currentExpandedIndex) =>{
+       if(currentExpandedIndex === nextIndex){
+        return(-1)
+    }else{
+        return nextIndex
+    }  
+    })
+}
 
 const renderedItems = items.map((item,index)=>{
     const isExpanded = index === expandedIndex
 
+    const icon = <span className="text-2xl">
+                    {isExpanded ? <GoChevronDown/> : <GoChevronLeft/>}
+                </span>
+
     return (
         <div key={item.id}>
-            <div>{item.label}</div>
-            {isExpanded && <div>{item.content}</div>}
+            <div className="flex items-center justify-between p-3 border-b cursor-pointer bg-gray-50" onClick={()=>handleClick(index)}>
+                {item.label}
+                {icon}
+            </div>
+            {isExpanded && <div className="p-5 border-b">{item.content}</div>}
         </div>
     )
 })
 
     return(
-        <div>{renderedItems}</div>
+        <div className="border-t rounded border-x">{renderedItems}</div>
     )
 }
 
