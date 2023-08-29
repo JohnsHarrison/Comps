16fa9322177511fc8b662ea1c05f9e31b5ebee80
John <jshar15089@gmail.com>
2023-08-29 04:53:28 -0400

    dropdown component created

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
-import Accordion from "./components/Accordion";
+import Dropdown from "./components/Dropdown";
 
 function App() {
-
-  const items = [
-    {
-      id:"fwfwef",
-      label:"Can I use react on a project",
-      content:"You can use React on any project you want."
-    },{
-      id:"asdasd2",
-      label:"Can I use Javascript on a project",
-      content:"You can use Javascript on any project you want."
-    },{
-      id:"awsdasdzxczxc",
-      label:"Can I use CSS on a project",
-      content:"You can use CSS on any project you want."
-    },
+  const options =[
+    {label: "Red", value: "red"},
+    {label: "Green", value: "green"},
+    {label: "Blue", value: "blue"},
   ]
 
-  return (
-    <div>
-      <Accordion items={items}/>
-    </div>
-  );
+
+return <Dropdown options={options} />
+
 }
 
 export default App;
